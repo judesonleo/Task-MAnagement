32d68ddad81ba2ee6bab25000b9267f2ee8740f2
Judeson Leo <judesonleo123@gmail.com>
2021-11-18 17:29:49 +0530

    first commit

--- Database/connect.js
+++ Database/connect.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 
 
 
-// const connectionstring='mongodb+srv://Judeson:Judeson143@nodeexpressprojects.6jttr.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
 
 const connectDB =(url)=>{
    return  mongoose.connect(url)
